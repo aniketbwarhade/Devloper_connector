f620fc29af2539466421d35c3267fabf49b0725a
Aniket Warhade <aniketwarhade0411@gmail.com>
2022-01-15 13:52:26 +0530

    Updated with client side code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,27 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-module.exports = function(req,res,next){
-    // Get the token from header
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
 
-    const token = req.header("x-auth-token");
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
 
-    // checkk if not token
-    if(!token){
-        return res.status(401).json({ msg: "No token , authorization denied "});
-    }
-
-    // verify token
-    try{
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
         req.user = decoded.user;
         next();
-
-    }catch(err){
-        res.status(401).json({msg: "Token is not valid "});
-    }
-}
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
